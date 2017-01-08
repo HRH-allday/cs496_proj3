8a7d0763ac80911490418057809b5b8f294f6fa9
Zeniuus <jsh56son@kaist.ac.kr>
2017-01-08 17:54:16 +0900

    게임오버 화면 팩맨 게임오버 화면 사용하는 방향으로 변경

--- app/src/main/java/com/example/q/wifitest/ClickFollowerGameOver.java
+++ app/src/main/java/com/example/q/wifitest/ClickFollowerGameOver.java
@@ -1,7 +1,10 @@
 package com.example.q.wifitest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -12,8 +15,20 @@ public class ClickFollowerGameOver extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clickfollower_gameover);
-        TextView textView = (TextView) findViewById(R.id.clickfollower_gameover_score);
-        textView.setText(Integer.toString(getIntent().getExtras().getInt("score")));
+        setContentView(R.layout.game_result_pacman);
+
+        TextView scoreText = (TextView) findViewById(R.id.score);
+        TextView highScore = (TextView) findViewById(R.id.highscore);
+
+        scoreText.setText("Score : " + Integer.toString(getIntent().getExtras().getInt("score")));
+        highScore.setText("Best Score : " + Integer.toString(getIntent().getExtras().getInt("score")));
+
+        Button btn = (Button) findViewById(R.id.restart);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
